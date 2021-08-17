9268d358bb2aa65f5adbb554af3c12b98089a71c
nicobazzoni <jillianamagsila@Jillians-iMac.fios-router.home>
2021-08-16 22:11:15 -0400

    commit tree 2402f8a2365bc19b4e16a25e5afc1fbeda59f46c

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+function mapToNegativize(rootArray){
+    return rootArray.map(x => x * -1);
+}
+
+function mapToNoChange(src){
+const newSrc = [...src]
+return newSrc
+}
+
+function mapToDouble(rootArr) {
+   return rootArr.map(x => x*2)
+}
+
+ function mapToSquare(root){
+    let newRoot = []
+     for (const num in root) {
+     const xx2 = root[num] **2
+     newRoot.push(xx2)
+ }
+    return newRoot
+}
+
+function reduceToTotal(root, begin=0) {
+    let all = begin
+    for (let i = 0; i < root.length; i++ ) {
+      all = all + root[i]
+    }
+    return all
+  }
+
+  function reduceToAllTrue(root) {
+    for (let i = 0; i < root.length; i++ ) {
+      if (!root[i]) return false
+    }
+    return true
+  }
+
+  function reduceToAnyTrue(rootArray){
+    const reducer = function(collector, currentValue){  
+        if(collector == true){
+            return true
+        } else {
+            return !!currentValue
+        } 
+    }
+    return rootArray.reduce(reducer, false)
+}
+
